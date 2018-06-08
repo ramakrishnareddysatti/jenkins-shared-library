b7486cf739a925e7c145fb8c082033fcf8f7b575
r.satti <r.satti@accenture.com>
2018-06-08 11:51:37 +0530

    added removeImages() method

--- vars/Utils.groovy
+++ vars/Utils.groovy
@@ -1,5 +1,21 @@
 
 
+def removeImages(artifactName) {
+		try {
+			sh 'docker rmi -f $(docker images -f "dangling=true" -q)'
+		} catch (err) {
+			echo "Trying to remove dangling Images: ${err}"
+		}
+
+		try {
+			//sh 'docker rmi -f $(docker images | grep "${artifactName}" | awk \"{print $3}\")'
+
+			sh "docker rmi -f $(docker images | grep ${artifactName} | awk \'{print $3}\')"
+		} catch (err) {
+			echo "Trying remove ${artifactName}: ${err}"
+		}
+}
+
 def getArtifact(dirName) {
 	def matcher = readFile("${dirName}/pom.xml") =~ '<artifactId>(.+?)</artifactId>'
 	matcher ? matcher[0][1] : null
@@ -9,6 +25,7 @@ def getReleasedVersion(dirName) {
 	def matcher = readFile("${dirName}/pom.xml") =~ '<version>(.+?)</version>'
     matcher ? matcher[0][1] : null 	
 }
+
 def sendNotification(buildStatus) {
 	//def mailRecipients = 'r.satti@accenture.com, sashi.kumar.sharma@accenture.com, shresthi.garg@accenture.com, suresh.kumar.sahoo@accenture.com';
 	def mailRecipients = 'r.satti@accenture.com'
@@ -23,6 +40,7 @@ def sendNotification(buildStatus) {
 		// notify users when the build is back to normal
 		emailext(
 				subject: "JENKINS Notification : Successful Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+				//  Generates beautiful email format. Since I didn't write the contents of "groovy-html.template", I am afraid to use
 				//body: '''${SCRIPT, template="groovy-html.template"}''',
                 body: """ <p>Successful: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p><p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>""",
 				recipientProviders: [culprits(), developers(), requestor(), brokenTestsSuspects(), brokenBuildSuspects(), upstreamDevelopers()],
@@ -35,6 +53,7 @@ def sendNotification(buildStatus) {
 		// notify users when the Pipeline fails
 		emailext(
 				subject: "JENKINS Notification : FAILED Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+				//  Generates beautiful email format. Since I didn't write the contents of "groovy-html.template", I am afraid to use
 				//body: '''${SCRIPT, template="groovy-html.template"}''',
                 body: """ <p>FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p><p>Check console output at "<a href="${env.BUILD_URL}">${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>"</p>""",
 				recipientProviders: [culprits(), developers(), requestor(), brokenTestsSuspects(), brokenBuildSuspects(), upstreamDevelopers()],
@@ -44,33 +63,4 @@ def sendNotification(buildStatus) {
 	}
 }
 
-/* Generats beautiful email format. Since I didn't write the contents of "groovy-html.template", I am afraid to use
-def sendNotification(String buildStatus) {
-	//def mailRecipients = 'r.satti@accenture.com, sashi.kumar.sharma@accenture.com, shresthi.garg@accenture.com, suresh.kumar.sahoo@accenture.com';
-	def mailRecipients = 'r.satti@accenture.com'
-	
-	// build status of null means success
-    buildStatus =  buildStatus ?: 'SUCCESS'
-	if (buildStatus == 'SUCCESS')
-	{
-		// notify users when the build is back to normal
-		emailext(
-				subject: "JENKINS Notification : Successful Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-				body: '''${SCRIPT, template="groovy-html.template"}''',
-				recipientProviders: [culprits(), developers(), requestor(), brokenTestsSuspects(), brokenBuildSuspects(), upstreamDevelopers()],
-				to: "${mailRecipients}",
-				replyTo: "${mailRecipients}"
-				)
-	}
-	else if (buildStatus == 'FAILED')
-	{
-		// notify users when the Pipeline fails
-		emailext(
-				subject: "JENKINS Notification : FAILED Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-				body: '''${SCRIPT, template="groovy-html.template"}''',
-				recipientProviders: [culprits(), developers(), requestor(), brokenTestsSuspects(), brokenBuildSuspects(), upstreamDevelopers()],
-				to: "${mailRecipients}",
-				replyTo: "${mailRecipients}"
-				)
-	}
-}*/
+
