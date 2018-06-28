0904413189a5b602ebbb6ea3c73c6da390a807a9
r.satti <r.satti@accenture.com>
2018-06-29 01:10:58 +0530

    updating removeImages utility method

--- vars/Utils.groovy
+++ vars/Utils.groovy
@@ -147,7 +147,7 @@ def removeImages(artifactName, tag) {
 	}
 
 	if (images != "" && imagesWithTag != "") {
-		sh "docker images -a | grep \"${artifactName}\" | grep \"${tag}\" | awk '{print $3}' | xargs --no-run-if-empty docker rmi "
+		sh "docker images -a | grep \" ${artifactName} \" | grep \" ${tag} \" | awk '{print $3}' | xargs --no-run-if-empty docker rmi "
 	}
 
 
