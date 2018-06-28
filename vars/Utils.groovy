596310bf4a0a7787b89e489cc6decb4b1f1b0557
r.satti <r.satti@accenture.com>
2018-06-28 22:24:48 +0530

    updating removeImages utility method

--- vars/Utils.groovy
+++ vars/Utils.groovy
@@ -117,10 +117,10 @@ def deployUIToDev(artifactName, releasedVersion, PROP_ENV) {
 
 def removeImages(artifactName) {
 
-	sh 'docker images -qf dangling=true | xargs --no-run-if-empty docker rmi'
+	sh 'docker images --no-trunc -qf dangling=true | xargs --no-run-if-empty docker rmi'
 	//sh 'docker image prune'
-	sh "docker ps -aqf 'name=${artifactName}' | xargs -I {} docker stop {}"
-	sh 'docker rmi $(docker images --filter=reference="demandplannerui" -q)'
+	sh "docker ps --no-trunc -aqf 'name=${artifactName}' | xargs -I {} docker stop {}"
+	sh "docker rmi $(docker images --no-trunc -qf 'name=${artifactName}')"
 
 	 //sh 'docker images | grep "${artifactName}" | awk '{print $3}' | xargs -L1 docker rmi'	 
 	 //sh "docker rmi $(docker images --filter=reference=${artifactName} -q)"
