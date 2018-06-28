39852fa7f5284b1d10e6c971f60f46e2a2925804
r.satti <r.satti@accenture.com>
2018-06-28 23:33:31 +0530

    updating removeImages utility method

--- vars/Utils.groovy
+++ vars/Utils.groovy
@@ -26,7 +26,7 @@ def sourceCodeCheckout(applicationDir, branchName, repoUrl, distroDirPath, distr
 	}
 }
 
-def build(applicationDir, artifactName, releasedVersion) {
+def uiDockerBuild(applicationDir, artifactName, releasedVersion) {
 	dir(applicationDir) {
 		echo "Starting Docker Image Creation..."
 		sh "docker build -t ${artifactName}:${releasedVersion} ."
