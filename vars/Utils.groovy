6de867ffa24aff42e217e095344ba8b1cff7526f
r.satti <r.satti@accenture.com>
2018-07-03 02:41:57 +0530

    added new utlity methods

--- vars/Utils.groovy
+++ vars/Utils.groovy
@@ -293,8 +293,8 @@ def removeDanglingImages(artifactName, destinationIP) {
 	try{
 		sh """
 			ssh -t centos@${destinationIP} 'sudo su && docker images --no-trunc -aqf dangling=true | xargs --no-run-if-empty docker rmi && 
-			docker images | grep SNAPSHOT | tr -s ' ' | cut -d ' ' -f 3 | xargs --no-run-if-empty docker rmi &&
-			sudo su && docker ps --no-trunc -aqf 'name=${artifactName}' | xargs -I {} docker stop {}' 
+			docker images | grep SNAPSHOT | tr -s \' \' | cut -d \' \' -f 3 | xargs --no-run-if-empty docker rmi &&
+			docker ps --no-trunc -aqf 'name=${artifactName}' | xargs -I {} docker stop {}' 
 			"""
 	} catch(error) {
 		echo "${error}"
